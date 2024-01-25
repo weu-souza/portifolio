749e41010b7261a4d8b910b074cdc95f86b7d331
weu-souza <weuller1561@gmail.com>
2024-01-25 17:14:30 -0300

    Adicionando Novo projeto

--- src/app/template/experiencia/model/experiencia.ts
+++ src/app/template/experiencia/model/experiencia.ts
@@ -11,11 +11,22 @@ export interface Iexperiencia {
 export const experiencia: Iexperiencia[] = [
   {
     titulo: 'Lanchonete',
-    cargo: 'Desenvolvedor full stack',
+    cargo: 'Desenvolvedor Full Stack',
     descricao: 'eu projetei o caso de uso, front end e e alguns componentes do back end, e um site voltado\n' +
       '        para vendas de comidas em Fast Food para facilitar o trabalho do cliente\n' +
       '        tanto na compra quanto venda de seu produto para saber mais vá em',
     linguagem: ['angular', 'scss', 'spring', 'Bootstrap'],
+    ano: 2023
+  },
+  {
+    titulo: 'Cook book',
+    cargo: 'Desenvolvedor Front End',
+    descricao: 'Desenvolvido com o objetivo de simplificar o processo de registro e a visualização de receitas,' +
+      ' o projeto Cook Book oferece uma experiência intuitiva e eficiente. ' +
+      'Com uma interface amigável, nossa plataforma foi projetada para tornar' +
+      ' o cadastro de suas receitas favoritas rápido e descomplicado. Além disso, proporciona uma visualização clara e acessível,' +
+      ' permitindo que você explore e reproduza suas criações culinárias com facilidade, para saber mais vá em',
+    linguagem: ['angular', 'scss', '.NET', 'PrimeNG'],
     ano: 2023
   },
 ]
--- src/app/template/sobre-mim/page/sobre-mim.component.ts
+++ src/app/template/sobre-mim/page/sobre-mim.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mim',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SobreMimComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
