d63a588a1b876fd83c24b469990b960313bd45e2
weu-souza <weuller1561@gmail.com>
2024-10-05 05:20:51 -0300

    feat: creating contact component, added global variable and mixins

--- src/app/Pages/contact/contact.component.ts
+++ src/app/Pages/contact/contact.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Contact } from './Api/model/contact';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+//@ts-ignore
+  contact:FormGroup
+  IContact:Contact = new Contact;
 
+  constructor(private fb: FormBuilder){
+
+  }
+  
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.contact = this.fb.group({
+      name:[''],
+      email:[''],
+      message:['']
+    })
+  }
+
+  send(){
+    this.IContact = Object.assign({}, this.IContact, this.contact.value);
+    console.log(this.IContact)
+  }
 }
