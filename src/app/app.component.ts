e0cf130c060bda2db26d27a423c72f0b4cd5a243
weu-souza <weuller1561@gmail.com>
2024-10-09 20:59:19 -0300

    fix: fixing navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, HeaderComponent,ToastModule],
+  imports: [CommonModule,RouterOutlet, HeaderComponent,ToastModule,RouterLink],
   providers:[MessageService,ToastServiceService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
