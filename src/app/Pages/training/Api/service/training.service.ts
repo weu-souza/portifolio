9cbd32b5e7da9ba721a5341e1b725f82e837a420
weu-souza <weuller1561@gmail.com>
2024-10-14 23:42:21 -0300

    feat: separating certifications from course group

--- src/app/Pages/training/Api/service/training.service.ts
+++ src/app/Pages/training/Api/service/training.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { TrainingClass, TrainingObj } from '../model/model';
+import { Certification, TrainingClass, TrainingObj } from '../model/model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrainingService {
   training: TrainingClass[] = TrainingObj;
+  certification:TrainingClass[] = Certification
   constructor() {}
 
   getTraining(): Observable<TrainingClass[]> {
     return of(this.training);
+  }
+  
+  getCertification(): Observable<TrainingClass[]> {
+    return of(this.certification);
   }
 
 }
