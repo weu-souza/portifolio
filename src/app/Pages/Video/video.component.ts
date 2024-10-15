02999904f66cc8bc3ae48472cad1e2c549ddc03e
weu-souza <weuller1561@gmail.com>
2024-10-14 23:27:15 -0300

    feat: added loading component to wait video loading

--- src/app/Pages/Video/video.component.ts
+++ src/app/Pages/Video/video.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Ivideo } from './Api/model/video';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-video',
@@ -13,21 +14,31 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './video.component.scss',
 })
 export class VideoComponent implements OnInit {
- @Input() title: string = '';
-  link:any = '';
+  @Input() title: string = '';
+  link: any = '';
+  loade:boolean = true;
   private sanitizer = inject(DomSanitizer);
-  video!:Ivideo[];
-  constructor(
-    private videoService: VideoService
-  ) {}
+  video!: Ivideo[];
+  constructor(private videoService: VideoService) {}
   ngOnInit(): void {
-      this.videoService.getVideo(this.title).subscribe((res)=>{
-      this.video = res; 
-      this.video.map((value) =>{
-        this.link = this.sanitizer.bypassSecurityTrustResourceUrl(
-          value.link
-        );
-      })
-     })
+    this.getvideo();
+  }
+
+   getvideo() {
+   
+    this.videoService.getVideo(this.title).subscribe((res) => {
+      this.video = res;
+      this.video.map((value) => {
+        this.link =  this.sanitizer.bypassSecurityTrustResourceUrl(value.link);
+
+        
+      });
+      
+      
+    });
+    
+  }
+  onIframeLoad(){
+    this.loade = false;
   }
 }
