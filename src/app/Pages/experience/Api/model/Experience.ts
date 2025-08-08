87c660af8feb07b3e358149a8ca5dbd3073415d2
weu-souza <weuller1561@gmail.com>
2025-08-08 19:26:29 -0300

    arrumando alguns leouts

--- src/app/Pages/experience/Api/model/Experience.ts
+++ src/app/Pages/experience/Api/model/Experience.ts
@@ -11,7 +11,7 @@ export const experiencia: Iexperiencia[] = [
     titulo: 'Compass.uol',
     cargo: 'Desenvolvedor Front End Estagio',
     descricao:
-      'Na compass eu participei de 3 projetos por 3 meses o primeiro com html css e javascript, o segundo react e typescript e o terceiro projeto react, typescript, vitest e deploy no ec2 e imagens num bucket do s3 da aws e os outros 3 meses foram estudando para conseguir a certificação aws cloud pratictioner, clf C02.',
+      'Na Compass, atuei no desenvolvimento de um sistema de e-commerce, utilizando React com TypeScript para construção da interface. Implementei testes unitários com Jest para garantir a qualidade do código e utilizei Tailwind CSS para estilização ágil e responsiva.  No deploy da aplicação, configurei uma  instância EC2 com Apache para hospedagem  na nuvem AWS, além de integrar um bucket S3  para armazenamento das imagens do projeto.  Minha experiência combina desenvolvimento front-end moderno com práticas de cloud computing, sempre buscando soluções eficientes e escaláveis.',
     linguagem: [
       'React',
       'Html',
@@ -23,5 +23,21 @@ export const experiencia: Iexperiencia[] = [
       'Scrum',
     ],
     ano: 2024,
+  },
+  {
+    titulo: 'Compass.uol',
+    cargo: 'Desenvolvedor Front End',
+    descricao:
+      'Migração de Aplicação VB6 para React + .NET Atuei na modernização de uma aplicação legada em VB6, migrando seu front-end para React com integração ao back-end em .NET.Responsável pelo layout (UI) e pela lógica do front-end, garantindo uma experiência fluida e alinhada com as necessidades do usuário. Colaborei na transição de uma arquitetura tradicional para uma solução moderna, melhorando a manutenibilidade e performance da aplicação.  Testes e Otimização em Google Cloud (Agentspace) (2 meses – Avenue Code) Integrei um time focado em análise e testes dos conectores do Agentspace (Google Cloud), identificando bugs e propondo melhorias. Elaborei relatórios técnicos detalhando configurações ideais para maximizar a eficiência da ferramenta. Contribuí para a qualidade do produto através de documentação clara e sugestões de otimização com base nos testes realizados.',
+    linguagem: [
+      'React',
+      'Html',
+      'Css',
+      'Javascript',
+      'Typescript',
+      'git & github',
+      'Scrum',
+    ],
+    ano: 2025,
   },
 ];
