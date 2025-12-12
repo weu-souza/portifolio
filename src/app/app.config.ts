4a12f491500512fa73f4f6cc6c8ba640a1063121
weu-souza <weuller1561@gmail.com>
2025-12-12 10:36:51 -0300

    arrumando dark mode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes,withHashLocation()),
-     provideAnimations(),
-    ]
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: 'Aura',                           
+      },
+    }),
+    provideZoneChangeDetection(),
+    provideRouter(routes, withHashLocation()),
+    provideAnimations(),
+  ],
 };
