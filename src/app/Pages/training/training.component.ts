ae56250701ef84810b49c2b807573b0e8fa9d751
weu-souza <weuller1561@gmail.com>
2026-04-19 03:47:58 -0300

    refactor:formation translation

--- src/app/Pages/training/training.component.ts
+++ src/app/Pages/training/training.component.ts
@@ -1,41 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { CourseCardComponent } from './component/course-card/course-card.component';
-import { TrainingService } from './Api/service/training.service';
 import { TrainingClass } from './Api/model/model';
+import { LanguageService } from 'app/shared/utils/service/language-service/language-service';
 
 @Component({
-    selector: 'app-training',
-    imports: [CommonModule, CourseCardComponent],
-    templateUrl: './training.component.html',
-    styleUrl: './training.component.scss'
+  selector: 'app-training',
+  imports: [CommonModule, CourseCardComponent],
+  templateUrl: './training.component.html',
+  styleUrl: './training.component.scss',
 })
 export class TrainingComponent implements OnInit {
   training: TrainingClass[] = [];
   certification: TrainingClass[] = [];
   english: TrainingClass[] = [];
-  constructor(private trainingService: TrainingService) {}
+  constructor(private langService: LanguageService) {}
+
+  t(key: string, params?: any) {
+    return this.langService.tFunction()(key, params);
+  }
 
   ngOnInit(): void {
     this.getItem();
     this.getCertification();
-    this.getEnglish(); 
+    this.getEnglish();
   }
 
   getItem() {
-    this.trainingService.getTraining().subscribe((res) => {
+    this.t('training.courses').subscribe((res: TrainingClass[]) => {
       this.training = res;
     });
   }
 
   getCertification() {
-    this.trainingService.getCertification().subscribe((res) => {
+    this.t('training.certifications').subscribe((res: TrainingClass[]) => {
       this.certification = res;
     });
   }
 
   getEnglish() {
-    this.trainingService.getEnglish().subscribe((res) => {
+    this.t('training.languages').subscribe((res: TrainingClass[]) => {
       this.english = res;
     });
   }
