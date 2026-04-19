b4b6cf8e004f9f5aa7f5a909d2e212551d503acd
weu-souza <weuller1561@gmail.com>
2026-04-19 20:12:40 -0300

    refactor:add environments and update build.yml

--- src/environments/environment.example.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    SERVICE_ID: 'your service id here',
-    API_KEY: 'your public api key here',
-    TEMPLATE_ID: 'your template id here',
-  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  SERVICE_ID: '',
+  API_KEY: '',
+  TEMPLATE_ID: '',
+};
